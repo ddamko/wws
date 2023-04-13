3e2b050408327f5f6db03664710cd892bf12d6b5
Derek Damko <ddamko@wwsires.com>
2023-04-13 15:59:44 -0700

    Temp hard coding Builder API key for Vercel deployment

--- apps/marketing/src/routes/[...catchall]/+page.server.ts
+++ apps/marketing/src/routes/[...catchall]/+page.server.ts
@@ -1,14 +1,14 @@
 // src/routes/[...catchall]/+page.server.js
 import { getContent } from '@builder.io/sdk-svelte';
-import { PUBLIC_BUILDER_API_KEY } from "$env/static/public";
+// import { PUBLIC_BUILDER_API_KEY } from "$env/static/public";
 
 export const load = async ({ params }) => {
   const urlPath = `/${params.catchall}`;
 
   // fetch your Builder content
   const content = await getContent({
-    model: 'page',
-    apiKey: PUBLIC_BUILDER_API_KEY,
+    model: "page",
+    apiKey: "a3c0473cfc3f4922a401bf0057bdf2c2",
     userAttributes: {
       urlPath,
     },
--- apps/marketing/src/routes/[...catchall]/+page.svelte
+++ apps/marketing/src/routes/[...catchall]/+page.svelte
@@ -1,7 +1,7 @@
 <!-- src/routes/[...catchall]/+page.svelte -->
 <script>
   import { RenderContent } from '@builder.io/sdk-svelte';
-  import { PUBLIC_BUILDER_API_KEY } from "$env/static/public";
+  // import { PUBLIC_BUILDER_API_KEY } from "$env/static/public";
   
   export let data;
   export let content = data.props.content;
@@ -10,7 +10,7 @@
 <main>
 {#if content}
   <!-- Render builder content with all required props -->
-  <RenderContent model="page" {content} apiKey={PUBLIC_BUILDER_API_KEY} />
+  <RenderContent model="page" {content} apiKey={"a3c0473cfc3f4922a401bf0057bdf2c2"} />
 {:else}
   Content Not Found
 {/if}
