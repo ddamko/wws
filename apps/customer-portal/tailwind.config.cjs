e3a1d6822a56e21dd5084631636e0e66bc216b8b
Derek Damko <ddamko@wwsires.com>
2023-04-13 08:49:29 -0700

    Install Skeleton UI lib and updated TailwindCSS config for Skeleton UI.

--- apps/customer-portal/tailwind.config.cjs
+++ apps/customer-portal/tailwind.config.cjs
@@ -1,11 +1,19 @@
 const config = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		require('path').join(require.resolve(
+			'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
 
 	theme: {
 		extend: {}
 	},
 
-	plugins: []
+	plugins: [
+		...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()
+	]
 };
 
 module.exports = config;
