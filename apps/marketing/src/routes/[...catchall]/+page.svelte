968b76a157d4e2765af52adde4e06db580b2398d
Derek Damko <ddamko@wwsires.com>
2023-04-14 09:06:12 -0700

    Registering Svelte component with Builder.io

--- apps/marketing/src/routes/[...catchall]/+page.svelte
+++ apps/marketing/src/routes/[...catchall]/+page.svelte
@@ -1,16 +1,33 @@
 <!-- src/routes/[...catchall]/+page.svelte -->
-<script>
-	import { getContent, RenderContent } from "@builder.io/sdk-svelte";
+<script lang="ts">
+	import { getContent, RenderContent, type RegisteredComponent } from "@builder.io/sdk-svelte";
+	import MyCounterButton from "ui/components/MyCounterButton.svelte";
 	// import { PUBLIC_BUILDER_API_KEY } from "$env/static/public";
 
 	export let data;
 	export let content = data.props.content;
+
+	const CUSTOM_COMPONENTS: RegisteredComponent[] = [
+		{
+      		component: MyCounterButton,
+      		name: 'Counter Button',
+			image: 'https://tabler-icons.io/static/tabler-icons/icons-png/3d-cube-sphere-off.png',
+			canHaveChildren: false,
+			inputs: [
+				{
+					name: 'text',
+					type: 'string',
+					defaultValue: 'Button Text',
+				}
+			]
+    	}
+	]
 </script>
 
 <main>
 	{#if content}
 		<!-- Render builder content with all required props -->
-		<RenderContent model="page" {content} apiKey={"a3c0473cfc3f4922a401bf0057bdf2c2"} />
+		<RenderContent model="page" {content} apiKey={"a3c0473cfc3f4922a401bf0057bdf2c2"} customComponents={CUSTOM_COMPONENTS} />
 	{:else}
 		Content Not Found
 	{/if}
--- packages/ui/components/MyCounterButton.svelte
+++ packages/ui/components/MyCounterButton.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  export let name: string;
+  
   let count = 0;
 
   function handleClick() {
@@ -7,5 +9,5 @@
 </script>
 
 <button type="button" class="btn variant-filled-primary" on:click="{handleClick}">
-  clicks: {count}
+  {name} {count}
 </button>
